bd62e2cbb0e3b3359d9f2eda5fdab731b73b13a1
Yoav David <yoavv.david2@gmail.com>
2024-12-14 17:38:46 +0200

    Added Recycler View

---
+++ app/src/main/java/com/example/studentsapp/StudentsRecyclerViewActivity.kt
@@ -0,0 +1,123 @@
+package com.example.studentsapp
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.studentsapp.model.Model
+import com.example.studentsapp.model.Student
+
+interface OnItemClickListener {
+    fun onItemClick(position: Int)
+    fun onItemClick(student: Student?)
+}
+
+class StudentsRecyclerViewActivity : AppCompatActivity() {
+    var students: MutableList<Student>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_students_recycler_view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // TODO: Step 1 - students_recycler_view XML file ✅
+        // TODO: Step 2 - Add Adapter ✅
+        // TODO: Step 3 - Add ViewHolder ✅
+
+        students = Model.shared.students
+
+        val recyclerView: RecyclerView = findViewById(R.id.students_recycler_view)
+        recyclerView.setHasFixedSize(true)
+
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+
+        val adapter = StudentsRecyclerAdapter(students)
+        adapter.listener = object: OnItemClickListener {
+            override fun onItemClick(position: Int) {
+                Log.d("Event", "On click Activity listener position $position")
+            }
+
+            override fun onItemClick(student: Student?) {
+                Log.d("Event", "On student clicked, name: ${student?.name}")
+            }
+        }
+        recyclerView.adapter = adapter
+    }
+
+    class StudentViewHolder(
+        itemView: View,
+        listener: OnItemClickListener?
+    ): RecyclerView.ViewHolder(itemView) {
+        private var nameTextView: TextView? = null
+        private var idTextView: TextView? = null
+        private var studentCheckBox: CheckBox? = null
+        private var student: Student? = null
+
+        init {
+            nameTextView = itemView.findViewById(R.id.student_row_name_text_view)
+            idTextView = itemView.findViewById(R.id.student_row_id_text_view)
+            studentCheckBox = itemView.findViewById(R.id.student_row_check_box)
+
+            studentCheckBox?.apply {
+                setOnClickListener {
+                    (tag as? Int)?.let { tag ->
+                        student?.isChecked = (it as? CheckBox)?.isChecked ?: false
+                    }
+                }
+            }
+
+            itemView.setOnClickListener {
+                Log.d("Event", "On click listener on position $adapterPosition")
+                listener?.onItemClick(student)
+            }
+        }
+
+        fun bind(student: Student?, position: Int) {
+            this.student = student
+            nameTextView?.text = student?.name
+            idTextView?.text = student?.id
+            studentCheckBox?.apply {
+                isChecked = student?.isChecked ?: false
+                tag = position
+            }
+        }
+    }
+
+    class StudentsRecyclerAdapter(private val students: MutableList<Student>?): RecyclerView.Adapter<StudentViewHolder>() {
+        var listener: OnItemClickListener? = null
+
+        override fun getItemCount(): Int = students?.size ?: 0
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
+            val itemView = LayoutInflater.from(parent.context).inflate(
+                R.layout.student_list_row,
+                parent,
+                false
+            )
+
+            return StudentViewHolder(itemView, listener)
+        }
+
+        override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
+            holder.bind(
+                student = students?.get(position),
+                position = position
+            )
+        }
+    }
+}
