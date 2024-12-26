4dd3b30db0421c6ceacc9ddb35110d969cdd976d
Yoav David <yoavv.david2@gmail.com>
2024-12-26 23:22:51 +0200

    Adding dependencies

--- app/src/main/java/com/example/studentsapp/MainActivity.kt
+++ app/src/main/java/com/example/studentsapp/MainActivity.kt
@@ -10,18 +10,6 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
-    var fragmentOne: StudentsListFragment? = null
-    var fragmentTwo: BlueFragment? = null
-    var fragmentThree: BlueFragment? = null
-    var fragmentFour: BlueFragment? = null
-
-    var buttonOne: Button? = null
-    var buttonTwo: Button? = null
-    var buttonThree: Button? = null
-    var buttonFour: Button? = null
-
-    var fragment : BlueFragment? = null
-    var inDisplayFragment: Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,62 +22,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Lesson 5
-        // TODO: Step 1 - Set MainActivity Launcher ✅
-        // TODO: Step 2 - Create fragment from xml ✅
-        // TODO: Step 3 - Create fragment programmatically ✅
-        // TODO: Step 4 - Manage nav args ✅
-        // TODO: Step 5 - Create a tab bar with multiple fragments
-        // TODO: Step 6 - Refactor students list
+        // TODO: Step 1 - Setup project for nav graphs
+        // TODO: Step 2 - Setup nav_graph.xml and add Destinations
+        // TODO: Step 3 - Setup MainActivity with nav host and nav controller
+        // TODO: Step 4 -
+        // TODO: Step 5 -
+        // TODO: Step 6 -
 
 
-        fragmentOne = StudentsListFragment()
-        fragmentTwo = BlueFragment.newInstance("²")
-        fragmentThree = BlueFragment.newInstance("³")
-        fragmentFour = BlueFragment.newInstance("⁴")
-
-        buttonOne = findViewById(R.id.main_activity_button_one)
-        buttonTwo = findViewById(R.id.main_activity_button_two)
-        buttonThree = findViewById(R.id.main_activity_button_three)
-        buttonFour = findViewById(R.id.main_activity_button_four)
-
-        buttonOne?.setOnClickListener {
-            addFragment(fragmentOne)
-        }
-
-        buttonTwo?.setOnClickListener {
-            addFragment(fragmentTwo)
-        }
-
-        buttonThree?.setOnClickListener {
-            addFragment(fragmentThree)
-        }
-
-        buttonFour?.setOnClickListener {
-            addFragment(fragmentFour)
-        }
     }
 
-    fun addFragment(fragment: Fragment?) {
-        fragment?.let {
-            supportFragmentManager.beginTransaction().apply {
-                add(R.id.main_activity_frame_layout, it)
-                inDisplayFragment?.let {
-                    remove(it)
-                }
-                addToBackStack("TAG")
-                commit()
-            }
-        }
-        inDisplayFragment = fragment
-    }
-
-    fun removeFragment() {
-        fragment?.let {
-            supportFragmentManager.beginTransaction().apply {
-                remove(it)
-                commit()
-            }
-        }
-        fragment = null
-    }
 }
