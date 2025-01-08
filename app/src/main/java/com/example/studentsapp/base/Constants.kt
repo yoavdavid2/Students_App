70951ebee858533d909bc4d275354446ee36dfe1
Yoav David <yoavv.david2@gmail.com>
2025-01-08 17:17:45 +0200

    Added delete function and created constants

---
+++ app/src/main/java/com/example/studentsapp/base/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.studentsapp.base
+
+import com.example.studentsapp.model.Student
+
+typealias StudentsCallback = (List<Student>) -> Unit
+typealias EmptyCallback = () -> Unit
+
+object Constants {
+}
--- app/src/main/java/com/example/studentsapp/model/Model.kt
+++ app/src/main/java/com/example/studentsapp/model/Model.kt
@@ -2,18 +2,19 @@ package com.example.studentsapp.model
 
 import android.os.Looper
 import androidx.core.os.HandlerCompat
+import com.example.studentsapp.base.EmptyCallback
+import com.example.studentsapp.base.StudentsCallback
 import com.example.studentsapp.model.dao.AppLocalDb
 import com.example.studentsapp.model.dao.AppLocalDbRepository
 import java.util.concurrent.Executors
-
-typealias StudentsCallback = (List<Student>) -> Unit
-typealias EmptyCallback = () -> Unit
 
 class Model private constructor() {
 
     private val database: AppLocalDbRepository = AppLocalDb.database
     private val executor = Executors.newSingleThreadExecutor()
     private val mainHandler = HandlerCompat.createAsync(Looper.getMainLooper())
+
+    private val firebaseModel = FirebaseModel()
 
     companion object {
         val shared = Model()
@@ -37,6 +38,16 @@ class Model private constructor() {
 
             Thread.sleep(4000)
 
+            mainHandler.post {
+                callback()
+            }
+        }
+    }
+
+    fun deleteStudent(student: Student, callback: EmptyCallback) {
+        executor.execute {
+            database.studentDao().delete(student)
+            Thread.sleep(4000)
             mainHandler.post {
                 callback()
             }
